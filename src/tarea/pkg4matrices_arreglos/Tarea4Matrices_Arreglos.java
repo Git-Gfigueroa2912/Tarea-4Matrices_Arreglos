733b63cbc62d5c84a6d3ff0baa5e97d1a47dd72b
Git-Gfigueroa2912 <german_figueroa@unitec.edu>
2025-12-08 21:04:50 -0600

    Tercer Avance

--- src/tarea/pkg4matrices_arreglos/Tarea4Matrices_Arreglos.java
+++ src/tarea/pkg4matrices_arreglos/Tarea4Matrices_Arreglos.java
@@ -27,7 +27,7 @@ public class Tarea4Matrices_Arreglos {
             
             switch (opcion){
                 
-                case 1:
+                case 1->{
                     System.out.println("Battleship");
                     
                     System.out.println("MODO DEL JUEGO");
@@ -35,7 +35,9 @@ public class Tarea4Matrices_Arreglos {
                     System.out.println("2. Dos jugadores");
                     System.out.println("Ingrese el modo del juego");
                     int modo = entrada.nextInt();
-                       
+                    System.out.println("Desea modo silencioso [1/2]");
+                    int silencioso = entrada.nextInt();
+                    
                     if (modo == 1){
                         char[][] tablero = new char[6][5];
                         for(int i = 0; i < 6; i++){
@@ -55,14 +57,14 @@ public class Tarea4Matrices_Arreglos {
                         System.out.println("Ingrese una columna: ");
                         int columna = entrada.nextInt();
                         
-                        if(fila < 0 || fila >= fila || columna < 0 || columna >= columna){
+                        if(fila < 0 || fila >= 6 || columna < 0 || columna >= 5){
                             System.out.println("La posicion esta fuera de rango");
                             continue;
                         }
                         intentos++;
                         
                         if(fila == barcoFila && columna == barcoColumna){
-                            System.out.println(" El barco se hundio en " + intentos + " intentos! ");
+                            System.out.println(" El barco se hundio");
                             tablero[fila][columna] = 'X';
                             hundido = true;
                         }else{
@@ -73,13 +75,122 @@ public class Tarea4Matrices_Arreglos {
                             for(int j = 0; j < 5; j++){
                                 System.out.print(tablero[i][j] + " ");
                             }
+                            System.out.println();
                         }
                     }
                 }
+                if (modo == 2){
+                    char[][] A = new char[6][5];
+                    char[][] B = new char[6][5];
                     
+                    for(int i = 0; i < 6; i++){
+                        for(int j = 0; j < 5; j++){
+                            A[i][j] = '*';
+                            B[i][j] = '*';
+                        }
+                    }
+                    
+                    A[0][0] = '*';
+                    A[1][0] = '*'; A[1][1] = '*'; A[1][2] = '*';
+                    A[2][0] = '*';
+                    A[3][0] = '*';
+                    A[4][0] = '*';
+                    A[5][0] = '*';
+                    
+                    B[1][1] = '*';
+                    B[2][1] = '*';
+                    B[3][0] = '*';
+                    B[4][1] = '*';
+                    B[5][0] = '*'; B[5][1] = '*'; B[5][2] = '*';
+                    
+                    System.out.println("Desea modo silencioso : ");
+                    System.out.println("1. Si");
+                    System.out.println("2. No");
+                    silencioso = entrada.nextInt();
+                    
+                    int daño1 = 0;
+                    int daño2 = 0;
+                    int turno = 1;
+                    boolean fin = false;
+                    
+                    while(!fin){
+                        if(turno == 1){
+                            System.out.println("Jugador 1 ataca");
+                            mostrar(B);
+                            
+                            if(silencioso == 2){
+                                mostrar(B);
+                                
+                            }
+                            System.out.println("Ingrese la fila: ");
+                            int fila = entrada.nextInt();
+                            System.out.println("Ingrese la columna: ");
+                            int columna = entrada.nextInt();
+                            
+                            if(fila < 0 || fila >= 6 || columna < 0 || columna >= 6){
+                                System.out.println("La posicion esta fuera de rango");
+                                continue;
+                                
+                            }
+                            if(B[fila][columna] == '*'){
+                                System.out.println("Daña al barco");
+                                daño1++;
+                            }else{
+                                System.out.println("Bomba al agua");
+                            }
+                            B[fila][columna] = 'X';
+                            
+                            if(daño1 >= 3){
+                                System.out.println("Jugador 1 gana");
+                                fin = true;
+                            }
+                            turno = 2;
+                        }
+                        else{
+                            System.out.println("Jugador 2 ataca");
+                            
+                            if(silencioso == 2){
+                                mostrar(A);
+                            }
+                            
+                            System.out.println("Ingrese la fila: ");
+                            int fila = entrada.nextInt();
+                            System.out.println("Ingrese la columna: ");
+                            int columna = entrada.nextInt();
+                            
+                            if(fila < 0 || fila >= 6 || columna < 0 || columna >= 6){
+                                System.out.println("La posicion esta fuera de rango");
+                                continue;
+                            }
+                            
+                            if(A[fila][columna] == '*'){
+                                System.out.println("Daña al barco");
+                                daño2++;
+                            }else{
+                                System.out.println("Bomba al agua");
+                                
+                            }
+                            A[fila][columna] = 'X';
+                            
+                            if(daño2 >= 3){
+                                System.out.println("Jugador 2 gana");
+                                fin = true;
+                            }
+                            turno = 1;
+                        }
+                    }
+                 }    
                         
-            }
-        }while (opcion != 0);
+            }     
+            case 2->{
+                 System.out.println("Busca Minas");  
+            }    
+        }
+     }while (opcion != 0);
+    }    
+    public static void mostrar(char[][] tablero){
+        
     }
+    
 }
 
